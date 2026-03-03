2625c92668e5d8c8c7c236c5c2de7f347a891b23
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2026-03-03 09:21:59 +0100

    01 schema

--- SQL_scripts/01_schema.sql
+++ SQL_scripts/01_schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE region (
 );
 
 -- USERS
-ATE TABLE app_user (
+CREATE TABLE app_user (
     id              BIGINT AUTO_INCREMENT PRIMARY KEY,
     username        VARCHAR(50)  NOT NULL UNIQUE,
     email           VARCHAR(100) NOT NULL UNIQUE,
